f690fa8d2fbb3f727dbe11d8e96eb419e91340cc
JojuGeorge <jojugeorge001@gmail.com>
2022-04-27 18:19:12 +0530

    test

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from "./logo.svg";
 import "./App.css";
+import File1 from "./components/File1";
 
 function App() {
   return (
     <div className="App">
       <h1>my react apppppp</h1>
+      <File1 />
     </div>
   );
 }
---
+++ src/components/File1.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function File1() {
+  return (
+    <div>
+      <h1>File 1 . js</h1>>
+    </div>
+  );
+}
+
+export default File1;
